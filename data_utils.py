f965d5704d22f1d5b6f8fcc185070b74d879d9ef
Phil Miranthis <philippe.miranthis@gmail.com>
2025-03-12 14:27:41 +0000

    Refactor MixedInputDataset to use decode_image and remove unused transformations

--- data_utils.py
+++ data_utils.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
+from torchvision.io import decode_image
 from PIL import Image
 
 
@@ -23,17 +23,14 @@ class MixedInputDataset(Dataset):
         Column name in DataFrame that contains target labels
     tabular_columns : list
         List of column names to use as tabular features
-    transform : callable, optional
-        Optional transform to be applied on images
     """
-    def __init__(self, df, img_dir, img_id_column, target_column, tabular_columns, transform=None):
+    def __init__(self, df, img_dir, img_id_column, target_column, tabular_columns):
         self.df = df
         self.img_dir = img_dir
         self.img_id_column = img_id_column
         self.target_column = target_column
         self.tabular_columns = tabular_columns
-        self.transform = transform
-        
+
     def __len__(self):
         return len(self.df)
     
@@ -41,12 +38,8 @@ class MixedInputDataset(Dataset):
         # Get image
         img_id = self.df.iloc[idx][self.img_id_column]
         img_path = os.path.join(self.img_dir, img_id)
-        image = Image.open(img_path).convert('RGB')
-        
-        # Apply transformations to the image
-        if self.transform:
-            image = self.transform(image)
-        
+        image = decode_image(img_path)
+            
         # Get tabular features
         tabular_features = torch.tensor(
             self.df.iloc[idx][self.tabular_columns].values.astype(np.float32)
@@ -85,32 +78,12 @@ def create_dataloaders(train_df, val_df, img_dir, img_id_column, target_column,
         List of column names to use as tabular features
     batch_size : int
         Batch size for dataloaders
-    num_workers : int
-        Number of worker processes for data loading
         
     Returns:
     --------
     tuple: (train_loader, val_loader)
         Training and validation data loaders
     """
-    # Define transforms
-    train_transform = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.RandomRotation(30),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
-        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    
-    val_transform = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
     
     # Create datasets with appropriate transforms
     train_dataset = MixedInputDataset(
@@ -119,7 +92,6 @@ def create_dataloaders(train_df, val_df, img_dir, img_id_column, target_column,
         img_id_column,
         target_column,
         tabular_columns,
-        transform=train_transform
     )
     
     val_dataset = MixedInputDataset(
@@ -128,7 +100,6 @@ def create_dataloaders(train_df, val_df, img_dir, img_id_column, target_column,
         img_id_column,
         target_column,
         tabular_columns,
-        transform=val_transform
     )
     
     # Create DataLoaders
@@ -136,7 +107,6 @@ def create_dataloaders(train_df, val_df, img_dir, img_id_column, target_column,
         train_dataset,
         batch_size=batch_size,
         shuffle=True,
-        num_workers=num_workers,
         pin_memory=True
     )
     
@@ -144,7 +114,6 @@ def create_dataloaders(train_df, val_df, img_dir, img_id_column, target_column,
         val_dataset,
         batch_size=batch_size,
         shuffle=False,
-        num_workers=num_workers,
         pin_memory=True
     )
     
